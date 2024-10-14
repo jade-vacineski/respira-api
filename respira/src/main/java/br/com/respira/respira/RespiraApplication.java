044f95f5d003d8b16d7b50cc962ee1995387becf
jade-vacineski <ajadne856@gmail.com>
2024-10-14 20:17:58 -0300

    chore: add Keycloak setup for local development

---
+++ respira/src/main/java/br/com/respira/respira/RespiraApplication.java
@@ -0,0 +1,13 @@
+package br.com.respira.respira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RespiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RespiraApplication.class, args);
+	}
+
+}
---
+++ respira/src/test/java/br/com/respira/respira/RespiraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.respira.respira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RespiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
